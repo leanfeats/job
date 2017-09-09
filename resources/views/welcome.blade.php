b76f98e17d59f22decfac3c17f7bf5629f0708c2
vagrant <vagrant@homestead>
2017-09-09 08:33:55 +0000

    test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    DONALD!
                 </div>
 
                 <div class="links">
