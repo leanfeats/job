88e4ad1588d0a1440a4328d069b41e31ebfca769
Donald Pritos <dpritos@gmail.com>
2017-09-10 06:45:14 +0000

    test change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    DONALD!
+                    DONALD! CHANGE!
                 </div>
 
                 <div class="links">
